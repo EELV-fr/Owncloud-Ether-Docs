70e6a9119a2b5009508ec79a1533892de892a3f9
root <root@sectio-aurea.org>
2013-03-30 15:56:14 +0100

    first modifications

--- ajax/link.php
+++ ajax/link.php
@@ -13,24 +13,4 @@ if(OC_Filesystem::is_file($filename)){
 		}
 	}
 }
-else{
-	if(!class_exists('OC')) {
-		try {
-			// OC < 4
-			include_once('../../../lib/base.php');
-		} catch(Exception $e) {
-			// OC >= 4
-			require_once('lib/base.php');
-		}
-	}
-	$host = OC_Appconfig::getValue('files_etherpad', 'files_etherpad_host','');	
-	if($host!='' && OC_Files::newFile($_REQUEST['dir'], stripslashes($_REQUEST['file']), 'file')) {
-		$link=$host.'/p/'.str_replace('.url','',stripslashes($_REQUEST['file']));
-		$content='[InternetShortcut]\nURL='.$link."\n";
-		if(OC_Filesystem::file_put_contents($filename, $content)){
-			echo $link;
-			exit();
-		}
-	}
-}
 echo '0';
--- appinfo/app.php
+++ appinfo/app.php
@@ -1,4 +1,6 @@
 <?php
+OCP\Util::addscript( 'files_etherpad', 'core-min');
+OCP\Util::addscript( 'files_etherpad', 'md5-min');
 OCP\Util::addscript( 'files_etherpad', 'loader');
 OCP\App::registerAdmin('files_etherpad','settings');
 ?>
