e877252df29f2b97f286fef5b55e91abc7bd334f
upload <upload@sectio-aurea.org>
2013-03-30 22:16:22 +0100

    fix for instant edit

--- js/loader.js
+++ js/loader.js
@@ -17,23 +17,17 @@ function FileToPad(dir,file){
     function name(link) {
       pad.url=link.split('\n')[1].substr(4);
       pad.show();
-      if($('tr').filterAttr('data-file', file)==null){
-        $('<tr data-file="'+file+'" data-type="file" data-mime="text/x-url"><td class="filename svg ui-draggable" style="background-image:url(/core/img/filetypes/txt-plain.png)"><input type="checkbox" original-title=""><a class="name" href="/?app=files&amp;getfile=download.php?file='+dir+'/'+file+'" title=""><span class="nametext">'+file+'</span></a></td><td class="filesize" title="183.4 kB" style="color:rgb(199,199,199)">0.2</td><td class="date"><span class="modified" title="September 28, 2012, 15:12" style="color:rgb(200,200,200)">le mois dernier</span></td></tr>')
-          .appendTo('#fileList');
-      }
     }
   );
 }
-/*
+
 FileToPad.prototype={  
   show:function(){
     $("#editor").hide();
     $('#content table').hide();
     $('#controls').hide();
     var oldcontent = $('#content').html();
-    var viewer = OC.linkTo('files_filetopad','filetopad.php')+'&dir='+pad.dir+'&file='+pad.file;
-    var nom = pad.file;
-    $('#content').html(oldcontent+'<div id="filetopad_bar" >'+nom.replace('.pad','')+'<a id="filetopad_close">x</a></div><iframe style="width:100%;height:90%;display:block;" id="filetopad_frame" src="'+pad.url+'"></iframe>');
+    $('#content').html(oldcontent+'<div id="filetopad_bar">Titel: <strong>'+pad.file.replace('.pad','')+'</strong>, Öffentlicher Link: <a href="'+pad.url+'">'+pad.url+'</a>'+'<a id="filetopad_close">x</a></div><iframe style="width:100%;height:90%;display:block;" id="filetopad_frame" src="'+pad.url+'"></iframe>');
     $('#pageWidthOption').attr('selected','selected');    
     $('#filetopad_bar').css('padding-left','30px');  
     
@@ -50,13 +44,12 @@ FileToPad.prototype={
       $('#filetopad_bar').remove();
   }
 };
-*/
 
 $(document).ready(function(){
   if(location.href.indexOf('files')!=-1) {
-  $('tr').filterAttr('data-type', 'file').filterExtension( 'pad').attr('data-mime','text/x-url');
-  $('tr').filterAttr('data-type', 'file').filterExtension( 'pad').children('td:first-child').css('background-image','url('+OC.imagePath('files_etherpad', 'pad.png')+')');
-    if(typeof FileActions!=='undefined'){
+    $('tr').filterAttr('data-type', 'file').filterExtension( 'pad').attr('data-mime','text/x-url');
+    $('tr').filterAttr('data-type', 'file').filterExtension( 'pad').children('td:first-child').css('background-image','url('+OC.imagePath('files_etherpad', 'pad.png')+')');
+    if (typeof FileActions!=='undefined'){
       FileActions.register('text/x-url',t('files_etherpad','Edit'), OC.PERMISSION_READ, '',function(filename){
         pad=new FileToPad($('#dir').val(),filename);
       });
@@ -92,9 +85,10 @@ $(document).ready(function(){
                   function(result){
                     if (result.status == 'success') {
                       var date=new Date();
+                      new FileToPad
                       FileList.addFile(filename+'.pad',0,date,false,false);
                       var tr=$('tr').filterAttr('data-file',filename+'.pad');
-                      tr.attr('data-mime','text/plain');
+                      tr.attr('data-mime','text/x-url');
                       tr.attr('data-id', result.data.id);
                       var path = $('#dir').val();
                       getMimeIcon('text/plain',function(path){
