278db746530fc5255ec92b43706419cdfe817ce6
Ernesto Ruge <mail@ernestoruge.de>
2013-04-01 10:23:12 +0200

    translation support, back to .url

--- appinfo/app.php
+++ appinfo/app.php
@@ -1,6 +1,6 @@
 <?php
 OCP\Util::addscript( 'files_etherpad', 'core-min');
 OCP\Util::addscript( 'files_etherpad', 'md5-min');
-OCP\Util::addscript( 'files_etherpad', 'loader');
+OCP\Util::addscript( 'files_etherpad', 'files_etherpad');
 OCP\App::registerAdmin('files_etherpad','settings');
 ?>
--- l10n/fr.php
+++ l10n/fr.php
@@ -1,3 +1,7 @@
 <?php $TRANSLATIONS = array(
 	'Edit' => '&Eacute;diter',
+	'Etherpad Host' => 'Etherpad Host',
+	'ex' => 'ex',
+	'Title' => 'Titel',
+	'public link' => 'public link'
 );
